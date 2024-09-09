bdb828d91e81be5023ccc5678f037367d5f24cbe
wontothree <prepared7913@naver.com>
2024-09-09 21:13:02 +0900

    feat : project refactoring

--- faceRecognition/src/encodeGenerator.py
+++ faceRecognition/src/encodeGenerator.py
@@ -5,42 +5,61 @@ import pickle
 
 from firebase import FireBase
 
-firebase = FireBase()
+class EncodeGenerator:
+    def __init__(self):
+        # Firebase 초기화
+        self.bucket = self.initialize_firebase()
 
-def findEncodings(imagesList):
-    encodeList = []
-    for img in imagesList:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
+    def initialize_firebase(self):
+        firebase = FireBase()
+        _, storage = firebase.initializeFirebase()
+        return storage.bucket()
     
-    return encodeList
+    def find_encodings(self, images_list):
+        encode_list = []
+        for img in images_list:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encode_list.append(encode)
+        return encode_list
+    
+    def load_images_from_folder(self, folder_path):
+        path_list = os.listdir(folder_path)
+        img_list = []
+        person_ids = []
+        for path in path_list:
+            img_list.append(cv2.imread(os.path.join(folder_path, path)))
+            person_ids.append(os.path.splitext(path)[0])
+        return img_list, person_ids
+    
+    def upload_images_to_firebase(self, folder_path):
+        path_list = os.listdir(folder_path)
+        for path in path_list:
+            file_name = os.path.join(folder_path, path)
+            blob = self.bucket.blob(file_name)
+            blob.upload_from_filename(file_name)
 
+    def save_encodings_to_file(self, encode_list_with_ids, file_name="./EncodeFile.p"):
+        with open(file_name, 'wb') as file:
+            pickle.dump(encode_list_with_ids, file)
+        print("File Saved")
 
-_, storage = firebase.initializeFirebase()
-bucket = storage.bucket()
+if __name__ == "__main__":
+    encodeGenerator = EncodeGenerator()
+    
+    # 로컬 폴더에서 이미지와 ID 불러오기
+    folder_path = '/Users/kevinliam/Desktop/Kevin’s MacBook Air/development/inbody-scale-ai/faceRecognition/imgs'
+    img_list, person_ids = encodeGenerator.load_images_from_folder(folder_path)
 
-# importing the mode images into a list
-folderPath = '/Users/kevinliam/Desktop/Kevin’s MacBook Air/development/inbody-scale-ai/faceRecognition/imgs'
-PathList = os.listdir(folderPath)
-imgList = []
-personIds = []
-for path in PathList:
-    imgList.append(cv2.imread(os.path.join(folderPath, path)))
-    personIds.append(os.path.splitext(path)[0])
+    # Firebase 스토리지에 이미지 업로드
+    encodeGenerator.upload_images_to_firebase(folder_path)
 
-    fileName = f'{folderPath}/{path}'
-    bucket = storage.bucket()
-    blob = bucket.blob(fileName)
-    blob.upload_from_filename(fileName)
+    # 얼굴 인코딩 생성
+    print("Encoding Started ...")
+    encode_list_known = encodeGenerator.find_encodings(img_list)
+    encode_list_known_with_ids = [encode_list_known, person_ids]
+    print("Encoding Complete")
 
-print("Encoding Started ...")
-encodeListKnown = findEncodings(imgList)
-encodeListKnownWithIds = [encodeListKnown, personIds]
-print("Encoding Complete")
-print(encodeListKnownWithIds)
-
-file = open("EncodeFile.p", 'wb')
-pickle.dump(encodeListKnownWithIds, file)
-file.close()
-print("File Saved")
+    # 인코딩과 ID를 파일로 저장
+    encodeGenerator.save_encodings_to_file(encode_list_known_with_ids)
+    
--- faceRecognition/src/main.py
+++ faceRecognition/src/main.py
@@ -5,103 +5,104 @@ import pickle
 
 from firebase import FireBase
 
-firebase = FireBase()
+if __name__ == "__main__":
+    firebase = FireBase()
 
-db, storage = firebase.initializeFirebase()
-bucket = storage.bucket()
+    db, storage = firebase.initializeFirebase()
+    bucket = storage.bucket()
 
-# Set camera capture
-cap = cv2.VideoCapture(0)  # Ensure you are using the correct camera index
-cap.set(3, 1280)  # Width resolution
-cap.set(4, 720)   # Height resolution 
+    # Set camera capture
+    cap = cv2.VideoCapture(0)  # Ensure you are using the correct camera index
+    cap.set(3, 1280)  # Width resolution
+    cap.set(4, 720)   # Height resolution 
 
-# locad the encoding file
-print("Loading Encode File ...")
-file = open('/Users/kevinliam/Desktop/Kevin’s MacBook Air/development/inbody-scale-ai/faceRecognition/src/EncodeFile.p', 'rb')
-encodeListWithKnownWithIds = pickle.load(file)
-file.close()
-encodeListKnown, personIds = encodeListWithKnownWithIds
-print("Encode File Loaded")
+    # locad the encoding file
+    print("Loading Encode File ...")
+    file = open('/Users/kevinliam/Desktop/Kevin’s MacBook Air/development/inbody-scale-ai/faceRecognition/src/EncodeFile.p', 'rb')
+    encodeListWithKnownWithIds = pickle.load(file)
+    file.close()
+    encodeListKnown, personIds = encodeListWithKnownWithIds
+    print("Encode File Loaded")
 
-modeType = 0
-counter = 0
-id = -1
-imgPerson = []
+    modeType = 0
+    counter = 0
+    id = -1
+    imgPerson = []
 
-while True:
-    success, img = cap.read()
+    while True:
+        success, img = cap.read()
 
-    if not success:
-        print("Failed to grab frame")
-        break
+        if not success:
+            print("Failed to grab frame")
+            break
 
-    # make smaller for computation 
-    # imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25) # reduce size 1/4
-    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # imgS
+        # make smaller for computation 
+        # imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25) # reduce size 1/4
+        imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # imgS
 
-    faceCurFrame = face_recognition.face_locations(imgS)
-    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+        faceCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
 
-    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        # print("matches", matches)
-        # print("faceDis", faceDis)
+        for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print("matches", matches)
+            # print("faceDis", faceDis)
 
-        matchIndex = np.argmin(faceDis)
-        # print("Match Index", matchIndex)
+            matchIndex = np.argmin(faceDis)
+            # print("Match Index", matchIndex)
 
-        if matches[matchIndex]:
-            # print("Known Face Detected")
-            # print(personIds[[matchIndex]])
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = 4 * y1, 4 * x2, 4 * y2, 4 * x1
+            if matches[matchIndex]:
+                # print("Known Face Detected")
+                # print(personIds[[matchIndex]])
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = 4 * y1, 4 * x2, 4 * y2, 4 * x1
 
-            id = personIds[matchIndex]
-            # print(id)
-            if counter == 0:
-                counter = 1
-                modeType = 1
+                id = personIds[matchIndex]
+                # print(id)
+                if counter == 0:
+                    counter = 1
+                    modeType = 1
 
-    if counter != 0:
+        if counter != 0:
 
-        if counter == 1:
-            # get the data from database
-            personInfo = db.reference(f'Person/{id}').get()
+            if counter == 1:
+                # get the data from database
+                personInfo = db.reference(f'Person/{id}').get()
+                print(personInfo)
+
+                # get the image from the storage
+                blob = bucket.get_blob(f'imgs/{id}.png')
+
+                if blob is not None:
+
+                    array = np.frombuffer(blob.download_as_string(), np.uint8)  
+
+                    imgPerson = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+
+                # update data of usage
+                ref = db.reference(f'Person/{id}')
+                personInfo['usage_count'] += 1
+                print(personInfo['usage_count'])
+                ref.child('usage_count').set(personInfo['usage_count'])
+            
+            # if personInfo is not None:
+            cv2.putText(img, str(personInfo['name']), (861, 125), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1) 
+
+            # if blob is not None:
+            # imgBackground[175 : 175 + 216, 909 : 909 + 216] = imgPerson
+            
             print(personInfo)
+            counter += 1
 
-            # get the image from the storage
-            blob = bucket.get_blob(f'imgs/{id}.png')
+        # Show the combined image
+        cv2.imshow("Face Recognition", img) # imgBackground
 
-            if blob is not None:
+        # Check for exit key (e.g., 'q')
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-                array = np.frombuffer(blob.download_as_string(), np.uint8)  
-
-                imgPerson = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
-
-            # update data of usage
-            ref = db.reference(f'Person/{id}')
-            personInfo['usage_count'] += 1
-            print(personInfo['usage_count'])
-            ref.child('usage_count').set(personInfo['usage_count'])
-        
-        # if personInfo is not None:
-        cv2.putText(img, str(personInfo['name']), (861, 125), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1) 
-
-        # if blob is not None:
-        # imgBackground[175 : 175 + 216, 909 : 909 + 216] = imgPerson
-        
-        print(personInfo)
-        counter += 1
-
-    # Show the combined image
-    cv2.imshow("Face Recognition", img) # imgBackground
-
-    # Check for exit key (e.g., 'q')
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release camera and close windows
-cap.release()
-cv2.destroyAllWindows()
+    # Release camera and close windows
+    cap.release()
+    cv2.destroyAllWindows()
